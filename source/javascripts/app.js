1fbb499ba3a4c3b11de0605b28c268b27c7794c6
David Myers <davidmyers9000@gmail.com>
2014-06-19 09:55:54 -0400

    separate bxslider js file for foundation 3

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-  $('.bxslider').bxSlider({
-    mode: 'fade',
-    captions: true,
-    auto: true,
-    pause: 5000,
-    autoHover: true,
-  });
-});
-
 $(document).foundation();
